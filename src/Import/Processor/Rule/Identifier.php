cb78f8e5866dddde571b65a15fe9673a9c00b073
Maximilian Salomon <salomon@zib.de>
2020-01-27 16:38:11 +0100

    OPUSVIER-4213 Update Namespace and reduce Test-Complexity

---
+++ src/Import/Processor/Rule/Identifier.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Processor
+ * @package     Opus\Processor\Rule
+ * @author      Maximilian Salomon <salomon@zib.de>
+ * @copyright   Copyright (c) 2020, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ */
+
+namespace Opus\Bibtex\Import\Processor\Rule;
+
+class Identifier implements RuleInterface
+{
+    private $identifierMap = [
+        'arxiv' => 'arxiv',
+        'doi' => 'doi',
+        'issn' => 'issn',
+        'isbn' => 'isbn'
+    ];
+
+    public function process($field, $value, $bibtexBlock)
+    {
+        $return = [false];
+        if (array_key_exists($field, $this->identifierMap)) {
+            $identifiers = [];
+            foreach ($bibtexBlock as $key => $value) {
+                if (array_key_exists($key, $this->identifierMap)) {
+                    $identifier = [
+                        'Value' => $value,
+                        'Type' => $this->identifierMap[$key]
+                    ];
+                    array_push($identifiers, $identifier);
+                }
+            }
+            $return = [
+                true,
+                'Identifier',
+                $identifiers
+            ];
+        }
+        return $return;
+    }
+}
