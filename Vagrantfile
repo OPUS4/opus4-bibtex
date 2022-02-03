f01bbb7cbf29cd0cbbbd79ac05e1abb320618551
haogatyp <gatermann@effective-webwork.de>
2022-02-03 17:48:20 +0100

    #20  Vagrant configuration.

---
+++ Vagrantfile
@@ -0,0 +1,51 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+$software = <<SCRIPT
+# Downgrade to PHP 7.1
+apt-add-repository -y ppa:ondrej/php
+apt-get -yq update
+apt-get -yq install php7.1
+
+# Install required PHP packages
+apt-get -yq install php7.1-dom
+apt-get -yq install php7.1-mbstring
+apt-get -yq install php7.1-curl
+apt-get -yq install php7.1-zip
+
+# Install required tools
+apt-get -yq install pandoc
+SCRIPT
+
+$composer = <<SCRIPT
+/vagrant/bin/install-composer.sh
+cd /vagrant
+bin/composer update
+SCRIPT
+
+$environment = <<SCRIPT
+if ! grep "cd /vagrant" /home/vagrant/.profile > /dev/null; then
+  echo "cd /vagrant" >> /home/vagrant/.profile
+fi
+if ! grep "PATH=/vagrant/bin" /home/vagrant/.bashrc > /dev/null; then
+  echo "export PATH=/vagrant/bin:$PATH" >> /home/vagrant/.bashrc
+fi
+SCRIPT
+
+$help = <<SCRIPT
+echo "Use 'vagrant ssh' to log into VM and 'logout' to leave it."
+echo "In VM use:"
+echo "'composer test' for running tests"
+echo "'composer update' to update dependencies"
+echo "'composer cs-check' to check coding style"
+echo "'composer cs-fix' to automatically fix basic style problems"
+SCRIPT
+
+Vagrant.configure("2") do |config|
+  config.vm.box = "bento/ubuntu-20.04"
+
+  config.vm.provision 'shell', inline: $software
+  config.vm.provision 'shell', privileged: false, inline: $composer
+  config.vm.provision 'shell', inline: $environment
+  config.vm.provision "Help", type: "shell", privileged: false, inline: $help
+end
---
+++ bin/install-composer.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+#
+# LICENCE
+# This code is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This code is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# @author      Thomas Urban <thomas.urban@cepharum.de>
+# @author      Jens Schwidder <schwidder@zib.de>
+# @copyright   Copyright (c) 2010-2016, OPUS 4 development team
+# @license     http://www.gnu.org/licenses/gpl.html General Public License
+
+#
+# Skript for installing composer and OPUS 4 dependencies.
+#
+# Call like this "bin/install-composer.sh ." from the base directory of the
+# OPUS 4 application to install composer.phar into the same directory and
+# install the necessary dependencies.
+#
+# Parameters:
+#   1) Directory for installing composer
+#
+
+set -e
+
+SCRIPT_NAME="$(basename "$0")"
+SCRIPT_NAME_FULL="`readlink -f "$0"`"
+SCRIPT_PATH="`dirname "$SCRIPT_NAME_FULL"`"
+
+
+BASEDIR="`dirname "$SCRIPT_PATH"`"
+
+# Don't run Composer as root - Composer itself warns against that
+if [[ $EUID -eq 0 ]]; then
+    echo -e "\nERROR: This script must not be run as root.\n" 1>&2
+    exit 1
+fi
+
+if [ -e composer ] ;
+then
+  exit 1
+fi
+
+EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet --install-dir="$BASEDIR/bin" --filename=composer
+RESULT=$?
+rm composer-setup.php
+exit $RESULT
+
